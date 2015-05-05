596b8d0594532dd039f822bbb8f68fa808327512
Fredrik Mellbin <fredrik.mellbin@gmail.com>
2015-05-05 12:56:08 +0200

    Fix 8bit minmum bug

--- src/neighbors/simd/minimum_sse2.c
+++ src/neighbors/simd/minimum_sse2.c
@@ -41,7 +41,7 @@ proc_8bit_sse2(uint8_t *buff, int bstride, int width, int height, int stride,
     uint8_t *orig = p0, *end = p2;
     uint8_t threshold = th > 255 ? 255 : (uint8_t)th;
 
-    line_copy8(p0, srcp, width + stride, 1);
+	line_copy8(p0, srcp + stride, width, 1);
     line_copy8(p1, srcp, width, 1);
 
     __m128i xth = _mm_set1_epi8((int8_t)threshold);
